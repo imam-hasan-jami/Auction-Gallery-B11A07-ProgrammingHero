40a6fbe0ffdf82e220f566a7fd9b872cb2d6df71
IMAM HASAN JAMI <156547296+imamhasanjami@users.noreply.github.com>
2025-04-09 23:05:52 +0600

    update the item table
    used conditional rendering on the border and modified few other things

--- Auction-Gallery-SPA/src/Item/Item.jsx
+++ Auction-Gallery-SPA/src/Item/Item.jsx
@@ -1,29 +1,34 @@
 import React from "react";
 import { GoHeart } from "react-icons/go";
+import { GoHeartFill } from "react-icons/go";
 
-const Item = ({ item }) => {
+const Item = ({ item, isLastRow = false }) => {
     return (
-        <div>
-            <tr>
-                <td>
-                    <div className="flex items-center gap-3">
-                        <div className="avatar">
-                            <div className="mask mask-squircle h-12 w-12">
-                                <img src={item.image} alt="Item Image" />
-                            </div>
+        <tr className={isLastRow ? "" : "border-b-2 border-[#E5E5E5]/50"}>
+            <td className="px-5 py-5">
+                <div className="flex items-center gap-3">
+                    <div className="avatar">
+                        <div className="h-15 w-15">
+                            <img src={item.image} alt="Item Image" />
                         </div>
-                        <div>{item.title || "Item Title"}</div>
                     </div>
-                </td>
-                <td>{item.currentBidPrice || "Current Bid Price"}</td>
-                <td>{item.timeLeft || "Remaining Time"}</td>
-                <th>
-                    <button className="btn btn-ghost btn-xs">
-                        <GoHeart size={20} />
-                    </button>
-                </th>
-            </tr>
-        </div>
+                    <div>{item.title || "Item Title"}</div>
+                </div>
+            </td>
+            <td className="py-5">
+                {item.currentBidPrice || "Current Bid Price"}
+            </td>
+            <td className="py-5">{item.timeLeft || "Remaining Time"}</td>
+            <th className="py-5">
+                <button className="ml-6">
+                    <GoHeart size={20} />
+                </button>
+
+                <button className="hidden">
+                    <GoHeartFill size={20} />
+                </button>
+            </th>
+        </tr>
     );
 };
 
--- Auction-Gallery-SPA/src/components/Items/Items.jsx
+++ Auction-Gallery-SPA/src/components/Items/Items.jsx
@@ -6,22 +6,32 @@ const Items = ({ itemsPromise }) => {
 
     return (
         <div>
-            <h1>Items length: {items.length}</h1>
+            <div className="mt-33">
+                <h1 className="font-sora font-medium text-[32px]">
+                    Active Auctions
+                </h1>
+                <p className="font-sora text-xl mt-5 mb-8">
+                    Discover and bid on extraordinary items
+                </p>
+            </div>
 
             <div className="overflow-x-auto">
-                <table className="table">
-                    {/* head */}
-                    <thead>
+                <table className="table table-fixed mb-28 bg-white rounded-3xl">
+                    <thead className="font-sora text-[18px] border-b-2 border-[#E5E5E5]/50">
                         <tr>
-                            <th>Items</th>
-                            <th>Current Bid</th>
-                            <th>Time Left</th>
-                            <th>Bid Now</th>
+                            <th className="w-3/6 px-5 pt-12 pb-5">Items</th>
+                            <th className="w-1/6 pt-12 pb-5">Current Bid</th>
+                            <th className="w-1/6 pt-12 pb-5">Time Left</th>
+                            <th className="w-1/6 pt-12 pb-5">Bid Now</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        {items.map((item) => (
-                            <Item key={item.id} item={item} />
+                    <tbody className="font-sora text-base">
+                        {items.map((item, index) => (
+                            <Item
+                                key={item.id}
+                                item={item}
+                                isLastRow={index === items.length - 1}
+                            />
                         ))}
                     </tbody>
                 </table>
